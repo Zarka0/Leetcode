f8f0c31812a9593d0d3df9da663f0fa3ad67c2e5
Zarka0 <95235087+Zarka0@users.noreply.github.com>
2024-07-23 14:30:38 +0530

    Time: 146 ms (49.87%), Space: 85.8 MB (40.08%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int maximumGap(std::vector<int>& nums) {
-        if (nums.size() < 2) return 0;
-
-        // Sort the array
-        std::sort(nums.begin(), nums.end());
+        if (nums.size() < 2){
+             return 0;
+        }
+        sort(nums.begin(), nums.end());
 
         int maxGap = 0;
-
-        // Calculate the maximum difference between successive elements
         for (int i = 1; i < nums.size(); ++i) {
-            maxGap = std::max(maxGap, nums[i] - nums[i - 1]);
+            maxGap = max(maxGap, nums[i] - nums[i - 1]);
         }
 
         return maxGap;
