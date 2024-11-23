3e5b7017f1e33681437e90e6730827d701d277c2
Zarka0 <95235087+Zarka0@users.noreply.github.com>
2024-11-23 11:48:03 +0530

    Time: 15 ms (22.51%), Space: 45.1 MB (20.94%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean isPalindrome(String s) {
-   s=s.toLowerCase();
-    s=s.replaceAll("[^a-zA-Z0-9]","");
-    
-    String rstr="";
-    for(int i=s.length()-1; i>=0; i--){
-        rstr=rstr+s.charAt(i);
-    }
-    
-    return rstr.equals(s);
+       s= s.replaceAll("[^a-zA-Z0-9]", "");
+        s=s.toLowerCase();
+        StringBuilder sb=new StringBuilder(s);
+        return sb.reverse().toString().equals(s);
+
     }
 }
