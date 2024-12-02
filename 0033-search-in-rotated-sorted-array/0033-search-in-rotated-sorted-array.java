aa1adda08bdeaa8bb8f74a4c9706264a068627b1
Zarka0 <95235087+Zarka0@users.noreply.github.com>
2024-12-02 15:55:44 +0530

    Time: 0 ms (100%), Space: 42.4 MB (8.88%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,42 +1,34 @@
 class Solution {
     public int search(int[] nums, int target) {
-       if(nums.length==1){
-        if(nums[0]==target){
-            return 0;
-       }
-       else{
+        if(nums.length==0 || nums==null) return -1;
+        int left=0;
+        int right=nums.length-1;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>nums[right]){
+                left=mid+1;
+
+            }else{
+                right=mid;
+            }
+        }
+        int start=left;
+        left=0;
+        right=nums.length-1;
+        if(target>=nums[start] && target<=nums[right]){
+            left=start;
+        }else{
+            right=start;
+        }
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
         return -1;
-       }
-       }   
-       int start=0, end=nums.length-1;
-       while(start<end){
-        int mid=(start+end)/2;
-        if(nums[mid]>nums[end]){
-            start=mid+1;
-        }
-        else{
-            end=mid;
-        }
-       }
-       int pivot=start;
-       start=0;
-       end=nums.length-1;
-       if(target>=nums[pivot] && target<=nums[end]){
-        start=pivot;
-       }else{
-        end=pivot-1;
-       }
-       while(start<=end){
-        int mid=(start+end)/2;
-        if(nums[mid]==target){
-            return mid;
-        }else if(nums[mid]>target){
-            end=mid-1;
-        }
-        else{
-            start=mid+1;
-        }
-       }
-    return -1;
-}
+    }
 }
